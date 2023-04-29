ce299ea5300241d8517eccfb4ce027d3a4485b41
Yan <yan@yanjobs.me>
2023-04-29 21:45:42 +0200

    me = dumbass

--- src/main/java/me/yanjobs/pitquickmathslunar/events/ChatEvent.java
+++ src/main/java/me/yanjobs/pitquickmathslunar/events/ChatEvent.java
@@ -1,15 +1,18 @@
-package me.yanjobs.pitquickmathslunar.events;
+package me.yanjobs.pitutils.events;
 
 import club.maxstats.weave.loader.api.event.ChatReceivedEvent;
 import club.maxstats.weave.loader.api.event.SubscribeEvent;
 
+import java.io.IOException;
+import java.util.Objects;
 import java.util.Timer;
 
 import java.util.TimerTask;
 import java.util.concurrent.ThreadLocalRandom;
-import java.util.concurrent.TimeUnit;
 
+import me.yanjobs.pitutils.utils.Config;
 import net.minecraft.client.Minecraft;
+
 public class ChatEvent {
     public static double eval(final String str) {
         return new Object() {
@@ -20,7 +23,8 @@ public class ChatEvent {
             }
 
             boolean eat(int charToEat) {
-                while (ch == ' ') nextChar();
+                while (ch == ' ')
+                    nextChar();
                 if (ch == charToEat) {
                     nextChar();
                     return true;
@@ -31,7 +35,8 @@ public class ChatEvent {
             double parse() {
                 nextChar();
                 double x = parseExpression();
-                if (pos < str.length()) throw new RuntimeException("Unexpected: " + (char)ch);
+                if (pos < str.length())
+                    throw new RuntimeException("Unexpected: " + (char) ch);
                 return x;
             }
 
@@ -39,58 +44,76 @@ public class ChatEvent {
             // expression = term | expression `+` term | expression `-` term
             // term = factor | term `*` factor | term `/` factor
             // factor = `+` factor | `-` factor | `(` expression `)` | number
-            //        | functionName `(` expression `)` | functionName factor
-            //        | factor `^` factor
+            // | functionName `(` expression `)` | functionName factor
+            // | factor `^` factor
 
             double parseExpression() {
                 double x = parseTerm();
                 for (;;) {
-                    if      (eat('+')) x += parseTerm(); // addition
-                    else if (eat('-')) x -= parseTerm(); // subtraction
-                    else return x;
+                    if (eat('+'))
+                        x += parseTerm(); // addition
+                    else if (eat('-'))
+                        x -= parseTerm(); // subtraction
+                    else
+                        return x;
                 }
             }
 
             double parseTerm() {
                 double x = parseFactor();
                 for (;;) {
-                    if      (eat('*')) x *= parseFactor(); // multiplication
-                    else if (eat('/')) x /= parseFactor(); // division
-                    else return x;
+                    if (eat('*'))
+                        x *= parseFactor(); // multiplication
+                    else if (eat('/'))
+                        x /= parseFactor(); // division
+                    else
+                        return x;
                 }
             }
 
             double parseFactor() {
-                if (eat('+')) return +parseFactor(); // unary plus
-                if (eat('-')) return -parseFactor(); // unary minus
+                if (eat('+'))
+                    return +parseFactor(); // unary plus
+                if (eat('-'))
+                    return -parseFactor(); // unary minus
 
                 double x;
                 int startPos = this.pos;
                 if (eat('(')) { // parentheses
                     x = parseExpression();
-                    if (!eat(')')) throw new RuntimeException("Missing ')'");
+                    if (!eat(')'))
+                        throw new RuntimeException("Missing ')'");
                 } else if ((ch >= '0' && ch <= '9') || ch == '.') { // numbers
-                    while ((ch >= '0' && ch <= '9') || ch == '.') nextChar();
+                    while ((ch >= '0' && ch <= '9') || ch == '.')
+                        nextChar();
                     x = Double.parseDouble(str.substring(startPos, this.pos));
                 } else if (ch >= 'a' && ch <= 'z') { // functions
-                    while (ch >= 'a' && ch <= 'z') nextChar();
+                    while (ch >= 'a' && ch <= 'z')
+                        nextChar();
                     String func = str.substring(startPos, this.pos);
                     if (eat('(')) {
                         x = parseExpression();
-                        if (!eat(')')) throw new RuntimeException("Missing ')' after argument to " + func);
+                        if (!eat(')'))
+                            throw new RuntimeException("Missing ')' after argument to " + func);
                     } else {
                         x = parseFactor();
                     }
-                    if (func.equals("sqrt")) x = Math.sqrt(x);
-                    else if (func.equals("sin")) x = Math.sin(Math.toRadians(x));
-                    else if (func.equals("cos")) x = Math.cos(Math.toRadians(x));
-                    else if (func.equals("tan")) x = Math.tan(Math.toRadians(x));
-                    else throw new RuntimeException("Unknown function: " + func);
+                    if (func.equals("sqrt"))
+                        x = Math.sqrt(x);
+                    else if (func.equals("sin"))
+                        x = Math.sin(Math.toRadians(x));
+                    else if (func.equals("cos"))
+                        x = Math.cos(Math.toRadians(x));
+                    else if (func.equals("tan"))
+                        x = Math.tan(Math.toRadians(x));
+                    else
+                        throw new RuntimeException("Unknown function: " + func);
                 } else {
-                    throw new RuntimeException("Unexpected: " + (char)ch);
+                    throw new RuntimeException("Unexpected: " + (char) ch);
                 }
 
-                if (eat('^')) x = Math.pow(x, parseFactor()); // exponentiation
+                if (eat('^'))
+                    x = Math.pow(x, parseFactor()); // exponentiation
 
                 return x;
             }
@@ -121,18 +144,17 @@ public class ChatEvent {
      * @param message the unformatted message containing quickMaths
      * @return the equation's result parsed from the message param
      */
-    public String result (String message) {
-        String expression = message.substring(message.lastIndexOf("&e") +2 , message.lastIndexOf("&r"));
+    public String result(String message) {
+        String expression = message.substring(message.indexOf(":") + 2);
         expression = expression.replace("x", "*");
         return String.valueOf((int) (eval(expression)));
     }
+
     @SubscribeEvent
-    public void onChatReceived(ChatReceivedEvent event) {
+    public void onChatReceived(ChatReceivedEvent event) throws IOException {
         String quickMathMessage = event.getMessage().getUnformattedText();
 
-        System.out.println(quickMathMessage); // For debug purposes
-
-        if (quickMathMessage.contains("&r&d&lQUICK MATHS! &r&7Solve: &r&e")) {
+        if (quickMathMessage.contains("QUICK MATHS! Solve: ")) {
             String result = result(quickMathMessage);
 
             // Creating a new task
@@ -147,6 +169,6 @@ public class ChatEvent {
             // Timing the task with the delay created above
             timer.schedule(task, delay);
         }
-    }
 
+    }
 }
